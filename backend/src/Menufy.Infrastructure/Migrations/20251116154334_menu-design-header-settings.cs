401fd5e6103eea196d3130b563a73542f619b7d2
Drkakashe <alihazemnassir@gmail.com>
2025-11-16 18:44:47 +0300

    menu design header settings migrations

---
+++ backend/src/Menufy.Infrastructure/Migrations/20251116154334_menu-design-header-settings.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Menufy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class menudesignheadersettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HeaderColor",
+                table: "MenuDesigns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HeaderDisplayMode",
+                table: "MenuDesigns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeaderImageUrl",
+                table: "MenuDesigns",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HeaderColor",
+                table: "MenuDesigns");
+
+            migrationBuilder.DropColumn(
+                name: "HeaderDisplayMode",
+                table: "MenuDesigns");
+
+            migrationBuilder.DropColumn(
+                name: "HeaderImageUrl",
+                table: "MenuDesigns");
+        }
+    }
+}
